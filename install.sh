87b05d7d518c711cbaa738bc99d42a421efcfb4e
Thanh Trung NGUYEN <flyingangel1987@gmail.com>
2018-09-23 02:17:16 +0200

    update installer, minor refactor

--- install.sh
+++ install.sh
@@ -1,6 +1,91 @@
 #!/usr/bin/env bash
 
+#check root right
+[[ $(id -u) -ne 0 ]] && echo "Please run as root" && exit 1
+
+#set working dir
 currentDir=$(dirname -- $(realpath $0))
 cd $currentDir
 
-#todo install service, loop interval, resolution, keyword, image type
+function askKeyword() {
+	read -r -p "Choose a keyword: " keyword
+
+	[[ -z $keyword ]] && echo "Empty keyword. Exiting now." && exit 1
+}
+
+function askQuality() {
+	local resolution
+	declare -a qualities
+
+	qualities+=("auto")
+	qualities+=("high")
+	qualities+=("medium")
+	qualities+=("ge:")
+	qualities+=("eq:")
+
+	question=$(
+		cat <<EOF
+Choose the image quality:
+1   auto        Image will be greater or equal than your screen resolution (default)
+2   high        High quality image
+3   medium      Medium quality image
+4   greater     Greater than a chosen size
+5   equal       Exactly equal a chosen size
+>
+EOF
+	)
+	read -r -p "$question" quality
+	echo
+
+	#default to quality 1
+	[[ -z $quality ]] && quality=1
+
+	if [[ $quality -eq 4 || $quality -eq 5 ]]; then
+		read -r -p "Choose a resolution (e.g 1920,1280): " resolution
+	fi
+
+	#empty string if quality auto
+	[[ $quality -eq 1 ]] && quality="" || quality="--quality=${qualities[$quality - 1]}"
+	quality="$quality$resolution"
+}
+
+function askInterval() {
+	declare -a presets
+
+	presets+=("*/15 * * * *")
+	presets+=("*/30 * * * *")
+	presets+=("*/60 * * * **")
+	presets+=("0 0 * * *")
+	presets+=("0 0 * * 0")
+
+	question=$(
+		cat <<EOF
+Change image every:
+1   15 minutes (default)
+2   30 minutes
+3   1 hour
+4   1 day
+5   1 week
+>
+EOF
+	)
+	read -r -p "$question" interval
+	echo
+
+	[[ -z $interval ]] && interval=1
+
+	interval=${presets[$interval - 1]}
+}
+
+#install dependancy
+git submodule update --init --recursive
+askKeyword
+askQuality
+askInterval
+
+line="$interval\troot\tbash $currentDir/randomizedesktop.sh $keyword $quality"
+echo -e "$line" >/etc/cron.d/randomize-desktop
+echo "Custom interval can be changed in /etc/cron.d/randomize-desktop"
+echo -e "\tnano /etc/cron.d/randomize-desktop"
+echo
+echo "Done"
--- randomizedesktop.sh
+++ randomizedesktop.sh
@@ -18,122 +18,128 @@
 
 DEBUG=true
 
+#necessary if script is not called from this folder
 source argparser/argparser.sh
 parse_args "$@"
 
-function isGNOME {
-    [[ $XDG_CURRENT_DESKTOP == GNOME ]] || [[ $XDG_CURRENT_DESKTOP == ubuntu:GNOME ]] || return 1
+function isGNOME() {
+	[[ $XDG_CURRENT_DESKTOP == GNOME ]] || [[ $XDG_CURRENT_DESKTOP == ubuntu:GNOME ]] || return 1
 }
 
-function isPLASMA {
-    [[ $XDG_CURRENT_DESKTOP == KDE ]] || return 1
+function isPLASMA() {
+	[[ $XDG_CURRENT_DESKTOP == KDE ]] || return 1
 }
 
 function reconf() {
-  readonly output=/tmp/plasma-org.kde.plasma.desktop-appletsrc
-  readonly config=~/.config/plasma-org.kde.plasma.desktop-appletsrc
-  [[ ! -e $output ]] || rm $output
-  while IFS= read line
-  do
-      if [[ $line != "Image=file://"* ]]; then
-        echo $line >> $output
-      else
-        rm ${line#*Image=file://}
-        echo "$1" >> $output
-      fi
-  done <"$config"
-  rm $config && mv $output ~/.config
-  kquitapp5 plasmashell
-  kstart plasmashell
+	readonly output=/tmp/plasma-org.kde.plasma.desktop-appletsrc
+	readonly config=~/.config/plasma-org.kde.plasma.desktop-appletsrc
+	[[ ! -e $output ]] || rm $output
+	while IFS= read line; do
+		if [[ $line != "Image=file://"* ]]; then
+			echo $line >>$output
+		else
+			rm ${line#*Image=file://}
+			echo "$1" >>$output
+		fi
+	done <"$config"
+	rm $config && mv $output ~/.config
+	kquitapp5 plasmashell
+	kstart plasmashell
 }
 
-function setPlasmaWall {
-  wget --directory-prefix=$HOME/.cache $1 2>/dev/null
-  wall=~/.cache/"${1##*/}"
-  reconf "Image=file://$wall"
+function setPlasmaWall() {
+	wget --directory-prefix=$HOME/.cache $1 2>/dev/null
+	wall=~/.cache/"${1##*/}"
+	reconf "Image=file://$wall"
 }
 
-function checkCompatibility {
-    if isGNOME; then
-        result="GNOME"
+function checkCompatibility() {
+	if isGNOME; then
+		result="GNOME"
 
-        elif isPLASMA; then
-        result="PLASMA"
-    else
-        echo "Unsupported system. This program requires Gnome or Plasma desktop"
-        return 1
-    fi
+	elif isPLASMA; then
+		result="PLASMA"
+	else
+		echo "Unsupported system. This program requires Gnome or Plasma desktop"
+		return 1
+	fi
 }
 
 #fetch image from google
-function fetchImageAsJSON {
-    resultArray=()
+function fetchImageAsJSON() {
+	resultArray=()
 
-    count=10
-    keyword="universe"
-    #todo convert space to url entities
-    useragent='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0'
-    [[ ! -z $1 ]] && link="www.google.com/search?q=${keyword}&tbs=isz:ex,iszw:$1,iszh:$2&tbm=isch" || link="www.google.com/search?q=${keyword}&tbm=isch"
-    imagelink=$(wget -e robots=off --user-agent "$useragent" -qO - "$link" | sed 's/</\n</g' | grep 'class="*rg_meta' | sed 's/">{"/">\n{"/g' | grep 'http' | head -n $count)
+	#todo
+	parseQuality $2
 
-    IFS=$'\n'
-    for i in $imagelink; do
-        resultArray+=("$i")
-    done
+	count=10
+	#todo convert space to url entities
+	useragent='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:62.0) Gecko/20100101 Firefox/62.0'
+	[[ ! -z $1 ]] && link="www.google.com/search?q=$1&tbs=isz:ex,iszw:$2,iszh:$3&tbm=isch" || link="www.google.com/search?q=$1&tbm=isch"
+	imagelink=$(wget -e robots=off --user-agent "$useragent" -qO - "$link" | sed 's/</\n</g' | grep 'class="*rg_meta' | sed 's/">{"/">\n{"/g' | grep 'http' | head -n $count)
+
+	IFS=$'\n'
+	for i in $imagelink; do
+		resultArray+=("$i")
+	done
 }
 
 #return images list as array
-function fetchImages {
-    local temp url
+function fetchImages() {
+	local temp url
 
-    fetchImageAsJSON $1 $2
+	fetchImageAsJSON $1 $2
 
-    images=()
+	images=()
 
-    #get url from JSON
-    for i in "${resultArray[@]}"; do
-        #fetch json key & value
-        temp=$(echo "$i" | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w "ou")
-        #fetch value
-        url=${temp##*|}
-        images+=("$url")
+	#get url from JSON
+	for i in "${resultArray[@]}"; do
+		#fetch json key & value
+		temp=$(echo "$i" | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w "ou")
+		#fetch value
+		url=${temp##*|}
+		images+=("$url")
 
-        if $DEBUG; then echo "Found $url"; fi
-    done
+		if $DEBUG; then echo "Found $url"; fi
+	done
 }
 
-function getScreenRes {
-  read res < <(cat /sys/class/graphics/fb0/virtual_size);
-  local w="${res%%,*}"
-  local h="${res#*,}"
-  fetchImages $w $h
+function getScreenRes() {
+	read res < <(cat /sys/class/graphics/fb0/virtual_size)
+	local w="${res%%,*}"
+	local h="${res#*,}"
+
+	quality="ge:$w,$h"
 }
 
-function pickRandomImage {
-    local size index
-
-    size=${#images[@]}
-    index=$((RANDOM % "$size"))
-    result=${images[$index]}
+function parseQuality() {
+	echo todo
 }
 
-function setDesktopBackground {
-    if isGNOME; then
-        gsettings set org.gnome.desktop.background picture-uri "$1"
-      elif isPLASMA; then
-        setPlasmaWall "$1"
-    fi
+function pickRandomImage() {
+	local size index
 
-    if $DEBUG; then echo "Set $1 as background"; fi
+	size=${#images[@]}
+	index=$((RANDOM % "$size"))
+	result=${images[$index]}
 }
 
+function setDesktopBackground() {
+	if isGNOME; then
+		gsettings set org.gnome.desktop.background picture-uri "$1"
+	elif isPLASMA; then
+		setPlasmaWall "$1"
+	fi
+
+	if $DEBUG; then echo "Set $1 as background"; fi
+}
 
 checkCompatibility || exit 1
 #fetch image return array of images
-if [[ -z $quality ]] && [[ -z $bestfit ]]; then
-  getScreenRes
-else
-  fetchImages
+if [[ -z $quality || $quality == "auto" ]]; then
+	getScreenRes
 fi
+
+fetchImages $argument1 $quality
 pickRandomImage
 setDesktopBackground "$result"
